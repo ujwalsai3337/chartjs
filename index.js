1f1d68957a12e9d8f5184e55fff4408d69049592
ujwalsai3337 <132985654+ujwalsai3337@users.noreply.github.com>
2024-02-29 23:42:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-
+import React from 'react';
 import './index.css';
-import React from "react";
-import { Pie,Line} from "react-chartjs-2";
-//import { Chart as ChartJS } from "chart.js/auto";
 
-function Body({ chartData }) {
-  return (
-    <div className='chart'>
-        <Line data={chartData} />
-        <Pie data={chartData} />
-  </div>);
+//import flatpickr from 'flatpickr';
+
+const InputSec = () => {
+
+
+
+    return (
+        <div className='input'>
+            <div className='input-field'>
+            <label className='label' htmlFor="year">Year:</label>
+            <select className="drop-input" name="year" id="year">
+                <option value="year1">I</option>
+                <option value="year2">II</option>
+                <option value="year3">III</option>
+                <option value="year4">IV</option>
+            </select>
+            </div>
+
+            <div className='input-field'>
+            <label className="label" htmlFor="domain">Domain:</label>
+            <select className='drop-input' name="domain" id="domain">
+                <option value="iot">IOT</option>
+                <option value="machine_learning">Machine Learning</option>
+                <option value="web_dev">Web Dev</option>
+                <option value="data_analytics">Data Analytics</option>
+            </select>
+            </div>
+
+            <div className='input-field' id="date-input">
+            <label className="label" htmlFor="date">Date:</label>
+            <input
+                className="drop-input"
+                type="date"
+                id="date"
+                name="date"
+                placeholder="Select Date"
+            />
+        </div>
+
+            
+        </div>
+    );
 }
 
-export default Body;
+export default InputSec;
