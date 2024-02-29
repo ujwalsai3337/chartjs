ff10d55e5f6b9871f49cc75c84ffabc50acfd8e0
ujwalsai3337 <132985654+ujwalsai3337@users.noreply.github.com>
2024-02-29 23:46:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
 import React from 'react';
 import './index.css';
-
-//import flatpickr from 'flatpickr';
-
-const InputSec = () => {
-
-
-
+import wantImage from './new.jpg';
+ 
+const Home = () => {
     return (
-        <div className='input'>
-            <div className='input-field'>
-            <label className='label' htmlFor="year">Year:</label>
-            <select className="drop-input" name="year" id="year">
-                <option value="year1">I</option>
-                <option value="year2">II</option>
-                <option value="year3">III</option>
-                <option value="year4">IV</option>
-            </select>
-            </div>
-
-            <div className='input-field'>
-            <label className="label" htmlFor="domain">Domain:</label>
-            <select className='drop-input' name="domain" id="domain">
-                <option value="iot">IOT</option>
-                <option value="machine_learning">Machine Learning</option>
-                <option value="web_dev">Web Dev</option>
-                <option value="data_analytics">Data Analytics</option>
-            </select>
-            </div>
-
-            <div className='input-field' id="date-input">
-            <label className="label" htmlFor="date">Date:</label>
-            <input
-                className="drop-input"
-                type="date"
-                id="date"
-                name="date"
-                placeholder="Select Date"
-            />
+        <div className='home'>
+            <nav className='nav-bar'>
+                <h1>Home</h1>
+                <h1>Analytics</h1>
+                <h1>Track Progress</h1>
+                <h1>View Progress</h1>
+                <img className='nav-img' src={wantImage} alt="noimage"/>
+            </nav>
         </div>
+    )
 
-            
-        </div>
-    );
 }
 
-export default InputSec;
+
+
+
+export default Home;
+
